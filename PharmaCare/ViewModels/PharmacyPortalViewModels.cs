c9439619bfcc359afc7e887929a03e472768897f
LUZI8 <161791203+LUZI8@users.noreply.github.com>
2026-08-26 07:04:48 +0300

    Show prescription requests in pharmacy portal

--- PharmaCare/ViewModels/PharmacyPortalViewModels.cs
+++ PharmaCare/ViewModels/PharmacyPortalViewModels.cs
@@ -4,9 +4,11 @@ public sealed class PharmacyPortalViewModel
 {
     public Pharmacy Pharmacy { get; set; } = null!;
     public List<MarketplaceOrder> Orders { get; set; } = new();
+    public List<MarketplacePrescriptionRequest> PrescriptionRequests { get; set; } = new();
     public List<PharmacyProduct> LowStock { get; set; } = new();
     public int PendingOrders { get; set; }
     public int PreparingOrders { get; set; }
+    public int PendingPrescriptionRequests { get; set; }
     public decimal RevenueToday { get; set; }
     public int ActiveProducts { get; set; }
 }
