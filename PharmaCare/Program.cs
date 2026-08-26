0c8e68ca8d1027b2cf3a8f07dac57235c7cde54c
LUZI8 <161791203+LUZI8@users.noreply.github.com>
2026-08-26 22:54:38 +0300

    Register marketplace operations service

--- PharmaCare/Program.cs
+++ PharmaCare/Program.cs
@@ -27,6 +27,7 @@ builder.Services.AddSingleton<IFileHelper, FileHelper>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<ICartRepository, CartRepository>();
 builder.Services.AddScoped<ISmartCartService, SmartCartService>();
+builder.Services.AddScoped<IMarketplaceOperationsService, MarketplaceOperationsService>();
 
 builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
 builder.Services.AddScoped<IEmailService, EmailService>();
