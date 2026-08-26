d4fc2291e196748461c6774b051e959df338f54b
LUZI8 <161791203+LUZI8@users.noreply.github.com>
2026-08-26 23:03:33 +0300

    Expose secure prescription files in pharmacy portal model

--- PharmaCare/ViewModels/PharmacyPortalViewModels.cs
+++ PharmaCare/ViewModels/PharmacyPortalViewModels.cs
@@ -5,6 +5,7 @@ public sealed class PharmacyPortalViewModel
     public Pharmacy Pharmacy { get; set; } = null!;
     public List<MarketplaceOrder> Orders { get; set; } = new();
     public List<MarketplacePrescriptionRequest> PrescriptionRequests { get; set; } = new();
+    public Dictionary<int, int> PrescriptionFileIds { get; set; } = new();
     public List<PharmacyProduct> LowStock { get; set; } = new();
     public int PendingOrders { get; set; }
     public int PreparingOrders { get; set; }
